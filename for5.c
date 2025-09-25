901e02aa0c2852657f52ee435e4f6105fb36acb1
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-09-25 19:02:06 +0530

    Factorial of a number

---
+++ for5.c
@@ -0,0 +1,11 @@
+// Factorial of a number
+
+#include <stdio.h>
+int main() {
+    int n=5, fact=1;
+    for(int i=1; i<=n; i++) {
+        fact *= i;
+    }
+    printf("Factorial = %d", fact);
+    return 0;
+}
