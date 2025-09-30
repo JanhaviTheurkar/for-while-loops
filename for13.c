83b58e9234cbedcad6e383e894bdd45f335ddc0a
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-09-30 19:52:01 +0530

    Fibonacci series (first 10 terms)

---
+++ for13.c
@@ -0,0 +1,13 @@
+// Fibonacci series (first 10 terms)
+
+#include <stdio.h>
+int main() {
+    int a=0, b=1, c;
+    for(int i=1; i<=10; i++) {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return 0;
+}
