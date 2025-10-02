b514e469cbd218064e5faabf43863c74aabe6c22
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-10-02 14:41:23 +0530

    Print pattern (triangle of stars)

---
+++ for18.c
@@ -0,0 +1,12 @@
+// Print pattern (triangle of stars)
+
+#include <stdio.h>
+int main() {
+    for(int i=1; i<=5; i++) {
+        for(int j=1; j<=i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
