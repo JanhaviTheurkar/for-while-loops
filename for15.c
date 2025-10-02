dc83493fae7e0a02b296efd14a998f762b08f0a0
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-10-02 14:39:42 +0530

    Sum of digits of a number

---
+++ for15.c
@@ -0,0 +1,11 @@
+// Sum of digits of a number
+
+#include <stdio.h>
+int main() {
+    int n=1234, sum=0;
+    for(; n>0; n/=10) {
+        sum += n%10;
+    }
+    printf("Sum of digits = %d", sum);
+    return 0;
+}
