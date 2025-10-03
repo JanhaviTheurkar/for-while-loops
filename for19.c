3102687aee1ad4fd7d8373fba98fe7af873cbf39
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-10-03 13:20:56 +0530

    Print pattern (reverse triangle)

---
+++ for19.c
@@ -0,0 +1,12 @@
+// Print pattern (reverse triangle)
+
+#include <stdio.h>
+int main() {
+    for(int i=5; i>=1; i--) {
+        for(int j=1; j<=i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
