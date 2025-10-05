b5b1d8e9237e18c5b5bcb9d42c09d9bb77cf0407
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-10-05 12:07:11 +0530

    Reverse a number

---
+++ wh7.c
@@ -0,0 +1,12 @@
+// 7. Reverse a number
+
+#include <stdio.h>
+int main() {
+    int n=1234, rev=0;
+    while(n!=0) {
+        rev = rev*10 + n%10;
+        n/=10;
+    }
+    printf("Reverse = %d", rev);
+    return 0;
+}
