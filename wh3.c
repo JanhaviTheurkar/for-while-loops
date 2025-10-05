a7a132948f3c54ee68beacc66787e85ede589b05
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-10-05 12:03:47 +0530

    Sum of first 10 natural numbers

---
+++ wh3.c
@@ -0,0 +1,12 @@
+// 3. Sum of first 10 natural numbers
+
+#include <stdio.h>
+int main() {
+    int i = 1, sum = 0;
+    while(i <= 10) {
+        sum += i;
+        i++;
+    }
+    printf("Sum = %d", sum);
+    return 0;
+}
