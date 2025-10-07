a9ab43ac49687b812bdd0b79b6d9788162a3b372
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-10-07 17:55:26 +0530

    Check palindrome number

---
+++ wh10.c
@@ -0,0 +1,13 @@
+// 10. Check palindrome number
+
+#include <stdio.h>
+int main() {
+    int n=121, rev=0, temp=n;
+    while(temp!=0) {
+        rev = rev*10 + temp%10;
+        temp/=10;
+    }
+    if(n==rev) printf("Palindrome");
+    else printf("Not Palindrome");
+    return 0;
+}
