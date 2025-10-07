4020965e5eb9d9c047fb24ce81142e2a37b1357b
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-10-07 17:56:03 +0530

    Print multiplication table of 5

---
+++ wh11.c
@@ -0,0 +1,11 @@
+// 11. Print multiplication table of 5
+
+#include <stdio.h>
+int main() {
+    int i=1;
+    while(i<=10) {
+        printf("5 x %d = %d\n", i, 5*i);
+        i++;
+    }
+    return 0;
+}
