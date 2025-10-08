042064c1a27e31262e5445f740556093cc1a9450
Janhavi Theurkar <janhavitheurkar@gmail.com>
2025-10-08 09:51:24 +0530

    Print square of numbers 1 to 10

---
+++ wh18.c
@@ -0,0 +1,11 @@
+// 18. Print square of numbers 1 to 10
+
+#include <stdio.h>
+int main() {
+    int i=1;
+    while(i<=10) {
+        printf("%d^2 = %d\n", i, i*i);
+        i++;
+    }
+    return 0;
+}
